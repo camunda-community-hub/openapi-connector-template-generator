b351a9219ef1267db7c47bda97360c7848d54743
hkupitz <hendrik.kupitz@alumni.fh-aachen.de>
2022-08-10 13:30:05 +0200

    Add OS selection because of OS-dependant classpath separator

--- generate.sh
+++ generate.sh
@@ -1,6 +1,11 @@
+# Set OS to "windows" or "unix" for the proper classpath separator
+os="unix"
+
 cd generators/connectors-codegen
 mvn package -DskipTests
 cd -
 
-java -cp generators/connectors-codegen/target/connectors-codegen-openapi-generator-1.0.0.jar:openapi-generator-cli.jar \
+[[ $os = "unix" ]] && separator=":" || separator=";"
+
+java -cp generators/connectors-codegen/target/connectors-codegen-openapi-generator-1.0.0.jar${separator}openapi-generator-cli.jar \
      org.openapitools.codegen.OpenAPIGenerator generate -g connectors-codegen -i in.yaml -o out
