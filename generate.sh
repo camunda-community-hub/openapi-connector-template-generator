6859b89090bfe1f1623f2a34321c2a57baa633db
Remco Westerhoud <remco@westerhoud.nl>
2022-08-11 09:21:37 +0200

    add fast-generate script

--- generate.sh
+++ generate.sh
@@ -1,12 +1,16 @@
 # Set OS to "windows" or "unix" for the proper classpath separator
 os="unix"
 
-cd generators/connectors-codegen
-mvn package -DskipTests
-cd -
+if [ -z $1 ]; then
+     cd generators/connectors-codegen
+     mvn package -DskipTests
+     cd -
+fi
+
 rm -rf .camunda/element-templates
 
 [[ $os = "unix" ]] && separator=":" || separator=";"
 
-java -DdebugOpenAPI -cp generators/connectors-codegen/target/connectors-codegen-openapi-generator-1.0.0.jar${separator}openapi-generator-cli.jar \
-     org.openapitools.codegen.OpenAPIGenerator generate -g connectors-codegen -i in.yaml -o .camunda/element-templates
+java -cp generators/connectors-codegen/target/connectors-codegen-openapi-generator-1.0.0.jar${separator}openapi-generator-cli.jar \
+     org.openapitools.codegen.OpenAPIGenerator generate -g connectors-codegen -i in.yaml -o .camunda/element-templates \
+     -t generators/connectors-codegen/src/main/resources/connectors-codegen
