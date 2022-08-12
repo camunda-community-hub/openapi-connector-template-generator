149f3283e59467bd3217f02ba0f3b00e5e7addc0
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2022-08-12 02:08:26 +0200

    Clean up code and folder structure + improve element template
    - Mustache templates are now stored in /src/main/resources/templates/
    - Outputted FEEL includes are now created in /.camunda/includes/
    - Outputted element templates are now created in /.camunda/element-templates
    - Rrequest body field for GET operations is now hidden (prevents potential error)
    - Request body + URL fields have been moved to separate group at the bottom

--- generators/connectors-codegen/src/main/java/io/camunda/connectors/codegen/ConnectorsCodegenGenerator.java
+++ generators/connectors-codegen/src/main/java/io/camunda/connectors/codegen/ConnectorsCodegenGenerator.java
@@ -1,162 +1,116 @@
 package io.camunda.connectors.codegen;
 
-import org.openapitools.codegen.*;
-import org.openapitools.codegen.model.*;
+import org.openapitools.codegen.CodegenConfig;
+import org.openapitools.codegen.CodegenOperation;
+import org.openapitools.codegen.CodegenType;
+import org.openapitools.codegen.DefaultCodegen;
+import org.openapitools.codegen.model.ModelMap;
+import org.openapitools.codegen.model.OperationMap;
+import org.openapitools.codegen.model.OperationsMap;
 
-import java.util.*;
 import java.io.File;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConnectorsCodegenGenerator extends DefaultCodegen implements CodegenConfig {
 
-  // source folder where to write the files
-  protected String sourceFolder = "src";
-  protected String apiVersion = "1.0.0";
-
-  /**
-   * Configures the type of generator.
-   *
-   * @return  the CodegenType for this generator
-   * @see     org.openapitools.codegen.CodegenType
-   */
-  public CodegenType getTag() {
-    return CodegenType.OTHER;
-  }
-
-  /**
-   * Configures a friendly name for the generator.  This will be used by the generator
-   * to select the library with the -g flag.
-   *
-   * @return the friendly name for the generator
-   */
-  public String getName() {
-    return "connectors-codegen";
-  }
-
-  /**
-   * Provides an opportunity to inspect and modify operation data before the code is generated.
-   */
-  @Override
-  public OperationsMap postProcessOperationsWithModels(OperationsMap objs, List<ModelMap> allModels) {
-
-    // to try debugging your code generator:
-    // set a break point on the next line.
-    // then debug the JUnit test called LaunchGeneratorInDebugger
-
-    OperationsMap results = super.postProcessOperationsWithModels(objs, allModels);
-
-    OperationMap ops = results.getOperations();
-    List<CodegenOperation> opList = ops.getOperation();
-
-    // iterate over the operation and perhaps modify something
-    for(CodegenOperation co : opList){
-      // example:
-      // co.httpMethod = co.httpMethod.toLowerCase();
-      setPathParamsInPath(co);
-    }
-
-    return results;
-  }
-
-  private void setPathParamsInPath(CodegenOperation co) {
-    co.pathParams.stream()
-            .map(param -> new AbstractMap.SimpleEntry<>("{" + param.paramName + "}", param))
-            .forEach(entry -> co.path = co.path.replace(entry.getKey(),
-                    String.format("\\\" + objects.%s_%s + \\\"/", co.operationId, entry.getValue().paramName)));
-
-  }
-
-  /**
-   * Returns human-friendly help for the generator.  Provide the consumer with help
-   * tips, parameters here
-   *
-   * @return A string value for the help message
-   */
-  public String getHelp() {
-    return "Generates a connectors-codegen client library.";
-  }
+  private String apiFolder = "element-templates";
+  private String modelFolder = "includes";
 
   public ConnectorsCodegenGenerator() {
     super();
 
-    // set the output folder here
-    outputFolder = "generated-code/connectors-codegen";
+    templateDir = "templates";
+    outputFolder = "generated";
 
-    /**
-     * Models.  You can write model files using the modelTemplateFiles map.
-     * if you want to create one template for file, you can do so here.
-     * for multiple files for model, just put another entry in the `modelTemplateFiles` with
-     * a different extension
-     */
-    modelTemplateFiles.put(
-      "models.mustache", // the template to use
-      ".feel");       // the extension for each file to write
+    modelTemplateFiles.put("model.mustache", ".feel");
+    apiTemplateFiles.put("api.mustache", ".json");
+  }
 
-    /**
-     * Api classes.  You can write classes for each Api file with the apiTemplateFiles map.
-     * as with models, add multiple entries with different extensions for multiple files per
-     * class
-     */
-    apiTemplateFiles.put(
-      "elementTemplate.mustache",   // the template to use
-      ".json");       // the extension for each file to write
+  public CodegenType getTag() {
+    return CodegenType.OTHER;
+  }
 
+  public String getName() {
+    return "connectors-codegen";
+  }
 
-    /**
-     * Template Location.  This is the location which templates will be read from.  The generator
-     * will use the resource stream to attempt to read the templates.
-     */
-    templateDir = "connectors-codegen";
+  public String getHelp() {
+    return "Generates element templates for use with Camunda Platform 8.";
+  }
+
+  @Override
+  public String modelFileFolder() {
+    return outputFolder + "/" + modelFolder + "/" + modelPackage().replace('.', File.separatorChar);
+  }
+
+  @Override
+  public String apiFileFolder() {
+    return outputFolder + "/" + apiFolder;
   }
 
   /**
-   * Escapes a reserved word as defined in the `reservedWords` array. Handle escaping
-   * those terms here.  This logic is only called if a variable matches the reserved words
+   * Modifies the operation data before the output is generated.
+   */
+  @Override
+  public OperationsMap postProcessOperationsWithModels(OperationsMap mapOfOperations, List<ModelMap> allModels) {
+
+    // For debugging: set a break point on the next line, then debug the JUnit test called "LaunchGeneratorInDebugger"
+    OperationsMap operationsWithImports = super.postProcessOperationsWithModels(mapOfOperations, allModels);
+    OperationMap operationsWithMetaInfos = operationsWithImports.getOperations();
+
+    List<ElementTemplateCodegenOperation> modifiedOperations = new ArrayList<>();
+    List<CodegenOperation> codegenOperations = operationsWithMetaInfos.getOperation();
+
+    for(CodegenOperation operation : codegenOperations){
+      setPathParamsInPath(operation);
+
+      ElementTemplateCodegenOperation modifiedOperation = new ElementTemplateCodegenOperation(operation);
+      modifiedOperations.add(modifiedOperation);
+    }
+
+    operationsWithMetaInfos.setOperation(modifiedOperations);
+
+    return operationsWithImports;
+  }
+
+  private void setPathParamsInPath(CodegenOperation operation) {
+    operation.pathParams.stream()
+            .map(param -> new AbstractMap.SimpleEntry<>("{" + param.paramName + "}", param))
+            .forEach(entry -> operation.path = operation.path.replace(entry.getKey(),
+                    String.format("\\\" + objects.%s_%s + \\\"/", operation.operationId, entry.getValue().paramName)));
+  }
+
+  /**
+   * Escapes a reserved word as defined in the `reservedWords` array.
+   * This logic is only called if a variable matches the reserved words.
    *
    * @return the escaped term
    */
   @Override
   public String escapeReservedWord(String name) {
-    return "_" + name;  // add an underscore to the name
+    return "_" + name;
   }
 
   /**
-   * Location to write model files.  You can use the modelPackage() as defined when the class is
-   * instantiated
-   */
-  public String modelFileFolder() {
-    return outputFolder + "/" + sourceFolder + "/" + modelPackage().replace('.', File.separatorChar);
-  }
-
-  /**
-   * Location to write api files.  You can use the apiPackage() as defined when the class is
-   * instantiated
-   */
-  @Override
-  public String apiFileFolder() {
-    return outputFolder;
-  }
-
-  /**
-   * override with any special text escaping logic to handle unsafe
-   * characters so as to avoid code injection
+   * Escapes unsafe characters to avoid code injection.
    *
    * @param input String to be cleaned up
-   * @return string with unsafe characters removed or escaped
+   * @return the string with unsafe characters removed or escaped
    */
   @Override
   public String escapeUnsafeCharacters(String input) {
-    //TODO: check that this logic is safe to escape unsafe characters to avoid code injection
-    return input;
+    return input; //TODO: Escape unsafe characters
   }
 
   /**
    * Escape single and/or double quote to avoid code injection
    *
    * @param input String to be cleaned up
-   * @return string with quotation mark removed or escaped
+   * @return the string with quotation marks escaped
    */
   public String escapeQuotationMark(String input) {
-    //TODO: check that this logic is safe to escape quotation mark to avoid code injection
     return input.replace("\"", "\\\"");
   }
 }
---
+++ generators/connectors-codegen/src/main/java/io/camunda/connectors/codegen/ElementTemplateCodegenOperation.java
@@ -0,0 +1,65 @@
+package io.camunda.connectors.codegen;
+
+import org.openapitools.codegen.CodegenOperation;
+
+public class ElementTemplateCodegenOperation extends CodegenOperation {
+  protected boolean isGET;
+
+  public ElementTemplateCodegenOperation(CodegenOperation o) {
+    super();
+
+    this.responseHeaders.addAll(o.responseHeaders);
+    this.hasAuthMethods = o.hasAuthMethods;
+    this.hasConsumes = o.hasConsumes;
+    this.hasProduces = o.hasProduces;
+    this.hasParams = o.hasParams;
+    this.hasOptionalParams = o.hasOptionalParams;
+    this.returnTypeIsPrimitive = o.returnTypeIsPrimitive;
+    this.returnSimpleType = o.returnSimpleType;
+    this.subresourceOperation = o.subresourceOperation;
+    this.isMap = o.isMap;
+    this.isArray = o.isArray;
+    this.isMultipart = o.isMultipart;
+    this.isResponseBinary = o.isResponseBinary;
+    this.hasReference = o.hasReference;
+    this.isRestfulIndex = o.isRestfulIndex;
+    this.isRestfulShow = o.isRestfulShow;
+    this.isRestfulCreate = o.isRestfulCreate;
+    this.isRestfulUpdate = o.isRestfulUpdate;
+    this.isRestfulDestroy = o.isRestfulDestroy;
+    this.isRestful = o.isRestful;
+    this.path = o.path;
+    this.operationId = o.operationId;
+    this.returnType = o.returnType;
+    this.httpMethod = o.httpMethod;
+    this.returnBaseType = o.returnBaseType;
+    this.returnContainer = o.returnContainer;
+    this.summary = o.summary;
+    this.unescapedNotes = o.unescapedNotes;
+    this.notes = o.notes;
+    this.baseName = o.baseName;
+    this.defaultResponse = o.defaultResponse;
+    this.discriminator = o.discriminator;
+    this.consumes = o.consumes;
+    this.produces = o.produces;
+    this.bodyParam = o.bodyParam;
+    this.allParams = o.allParams;
+    this.bodyParams = o.bodyParams;
+    this.pathParams = o.pathParams;
+    this.queryParams = o.queryParams;
+    this.headerParams = o.headerParams;
+    this.formParams = o.formParams;
+    this.authMethods = o.authMethods;
+    this.tags = o.tags;
+    this.responses = o.responses;
+    this.imports = o.imports;
+    this.examples = o.examples;
+    this.externalDocs = o.externalDocs;
+    this.vendorExtensions = o.vendorExtensions;
+    this.nickname = o.nickname;
+    this.operationIdLowerCase = o.operationIdLowerCase;
+    this.operationIdCamelCase = o.operationIdCamelCase;
+
+    this.isGET = o.httpMethod.equals("GET");
+  }
+}
