87420c387b0c6047edde762fa676068473bca7f6
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2022-08-12 14:30:52 +0200

    Add optional prefixing of element template

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 # Set OS to "windows" or "unix" for the proper classpath separator
-os="unix"
+os="windows"
 
-if [ -z $1 ]; then
+if [ $# -ge 1 ]; then
      cd generators/connectors-codegen
      mvn package -DskipTests
      cd -
@@ -11,7 +11,7 @@ rm -rf .camunda
 
 [[ $os = "unix" ]] && separator=":" || separator=";"
 
-java -DdebugModels -cp generators/connectors-codegen/target/connectors-codegen-openapi-generator-1.0.0.jar${separator}openapi-generator-cli.jar \
+java -cp generators/connectors-codegen/target/connectors-codegen-openapi-generator-1.0.0.jar${separator}openapi-generator-cli.jar \
      org.openapitools.codegen.OpenAPIGenerator generate -g connectors-codegen -i in.yaml -o .camunda \
      -t generators/connectors-codegen/src/main/resources/templates
 
@@ -19,12 +19,32 @@ for FILE in .camunda/includes/*.feel; do
      FILENAME="${FILE##*/}"
      REPLACE=\<${FILENAME%.*}\>
      CONTENTS=`cat $FILE`
+     echo Including FEEL template from $FILENAME in...
 
      for APIFILE in .camunda/element-templates/*.json; do
+          echo - $APIFILE
+
           if [ $os = "unix" ]; then
-            sed -i '' "s@$REPLACE@$CONTENTS@g" $APIFILE
+              sed -i '' "s@$REPLACE@$CONTENTS@g" $APIFILE
           else
-            sed -i "s@$REPLACE@$CONTENTS@g" $APIFILE
+              sed -i "s@$REPLACE@$CONTENTS@g" $APIFILE
           fi
      done
 done
+
+if [ $# -eq 2 ]; then
+    for APIFILE in .camunda/element-templates/*.json; do
+        BASEPATH="${APIFILE%/*}"
+        FILENAME="${APIFILE##*/}"
+
+        if [ $os = "unix" ]; then
+            sed -i '' "s@<PREFIX>@$2 - @g" $APIFILE
+        else
+            sed -i "s@<PREFIX>@$2 - @g" $APIFILE
+        fi
+
+        mv $APIFILE ${BASEPATH}/${2}_${FILENAME}
+    done
+fi
+
+read -p "Quit"
